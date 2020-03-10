6debecefc4f32ced4f78d0865273542f80e1c1bf
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-10 16:35:01 +0700

    changed todoList become array of Object

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
+
 let inputValue = document.getElementById('todoInput');
+let allButtonValue = document.getElementById('todoInput');
+let countToDoList = document.getElementById('totalToDoList');
+
+// when call addItem => sum of value of inputValue
+
+// console.log(sumOfToTalToDoListValue);
 
 let todoList = [];
+let sumOfToDoValue = [];
 
 let addItem = ()=>{
     let todoValue = inputValue.value;
-    todoList.push(todoValue);
+    todoList.push({
+        text: todoValue,
+        isDone: false
+        })
+        console.log(todoList)
+    let sumOfToDoValue = todoList.length;
+    countToDoList.innerHTML = `${sumOfToDoValue}`;
+    console.log(sumOfToDoValue);
     render();
-    // let htmlToDoArray = todoList.map((item,index)=>{
-    //     return `<li> ${item} <button onclick="removeItem(${index})">x</button> </li>`
-    // }).join('');
-
-    // document.getElementById("resultArea").innerHTML = htmlToDoArray;
-
 }
 let removeItem = (index)=> {
     todoList.splice(index,1)
     render();
-    
 }
+
 let render =()=>{
     let htmlToDoArray = todoList.map((item,index)=>{
-        return `<li> ${item} <button onclick="removeItem(${index})">x</button> </li>`
+        return `<li> ${item.text} <button onclick="removeItem(${index})">x</button> </li>`
     }).join('');
     document.getElementById("resultArea").innerHTML = htmlToDoArray;
 }
