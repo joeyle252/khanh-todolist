15070719451cccf7efaf0607a9dcc0b78bc1ed0d
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-10 23:30:00 +0700

    add undone and done function to render

--- main.js
+++ main.js
@@ -3,53 +3,72 @@ let inputValue = document.getElementById('todoInput');
 
 let allButtonValue = document.getElementById('todoInput');
 
-let countToDoList = document.getElementById('totalToDoList');
+// let countToDoList = document.getElementById('totalToDoList');
 
 let todoList = [];
 
 let sumOfToDoValue = [];
 
-let addItem = ()=>{
+let addItem = () => {
     let todoValue = inputValue.value;
     todoList.push({
         text: todoValue,
         isDone: false
-        })
-        console.log(todoList)
-    let sumOfToDoValue = todoList.length;
-    countToDoList.innerHTML = `${sumOfToDoValue}`;
-    console.log(sumOfToDoValue);
+    })
+    
     render();
 }
-let removeItem = (index)=> {
-    todoList.splice(index,1)
+let removeItem = (index,event) => {
+    event.stopPropagation();
+    todoList.splice(index, 1)
+
     render();
 }
 
-let toggle =(index)=>{
-     todoList[index].isDone = !(todoList[index].isDone);
-     console.log("khanh",todoList);
-     render();
+let toggle = (index) => {
+    todoList[index].isDone = !(todoList[index].isDone);
+    console.log("khanh", todoList);
+    render();
 }
 
-// let undone = ()=>{
-//     let isDone = todoList[index].isDone;
-//     let undoneArray = todoList.filter(index){
-//         if (isDone === false){
-//             return true
-//         } return false
-//     }
-//     }
-
+let done = ()=> {
+    const doneArr = todoList.filter((value) => {
+        if (value.isDone === true) {
+            return true
+        } else {
+            return false
+        }
+    })
+    return doneArr.length;
+}
+let unDone = ()=> {
+    const unDoneArr = todoList.filter((value) => {
+        if (value.isDone === false) {
+            return true
+        } else {
+            return false
+        }
+    })
+    return unDoneArr.length;
+}
 
 let render =()=>{
-     
+           
     let htmlToDoArray = todoList.map((item,index)=>{
-        if(item.isDone == false){
-            return `<li onclick="toggle(${index})"> ${item.text} <button onclick="removeItem(${index})">x</button> </li>`
+        if(item.isDone == false){   
+             return `<li onclick= "toggle(${index})"> ${item.text} <button onclick="removeItem(${index},event)">x</button> </li>`
         } else {
-            return  `<li onclick="toggle(${index})"> ${item.text} <button onclick="removeItem(${index})">x</button> </li>`.strike();
-        }
+            return  `<li onclick="toggle(${index})"> ${item.text} <button onclick="removeItem(${index},event)">x</button> </li>`.strike();
+        }  
     }).join('');
     document.getElementById("resultArea").innerHTML = htmlToDoArray;
+    let totalsHtml = ` 
+        <button onclick="allButton()">Total</button>
+        <p id="totalToDoList">${todoList.length}</p>
+        <button>Undone</button>
+        <p>${unDone()}</p>
+        <button>Done</button>
+        <p>${done()}</p>
+    `;
+    document.querySelector(".totalArea").innerHTML = totalsHtml;
 }
